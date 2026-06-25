b5876251008d1f0e0bfd060ebd74a92499cc8c5a
Goldozilla <hamizabhinawa@gmail.com>
2026-06-25 15:36:49 +0700

    Update hospital_app.py

--- hospital_app.py
+++ hospital_app.py
@@ -18,7 +18,7 @@ footer {visibility: hidden;}
 
 @st.cache_resource
 def load_model():
- with open('hospital_model.pkl", "rb') as f:
+ with open('hospital_model.pkl', 'rb') as f:
    return pickle.load(f)
 
 bundle = load_model()
